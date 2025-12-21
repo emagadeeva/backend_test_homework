03a13c7c45d6c4eec9189dde70e36e31d2f2f5ce
Екатерина Магадеева <emagadeeva@yahoo.com>
2025-12-21 20:25:51 +0100

    Add new file

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   program.py
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        test.txt
 print('Я домашка'
